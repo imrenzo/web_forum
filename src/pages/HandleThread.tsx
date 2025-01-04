fe7a4eae6df1abb081f1f45ad903aec9bdcdcf46
imrenzo <109291780+imrenzo@users.noreply.github.com>
2025-01-05 00:38:40 +0900

    added delete  operation

--- src/pages/HandleThread.tsx
+++ src/pages/HandleThread.tsx
@@ -1,27 +1,60 @@
-import { FormEvent, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { FormEvent, useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import Header from "../components/header";
 import { Thread } from "../types/types";
-import { ValidateThreadInput, PostThread } from "../contollers/controllers";
+import { ValidateThreadInput, PostThread, DeleteThread } from "../apiService/apiService";
 import NotFound from "./notFound";
 import { PageBoxStyle } from "../components/stylesheet";
 
 import { Box, Button } from "@mui/material";
 import TextField from '@mui/material/TextField';
 
+const methods = ["create", "update", "delete"];
+const methodsFunctions = [HandleCreateThread, HandleUpdateThread, HandleDeleteThread];
 
-export function CreateThread() {
-    /////////////////////// CHANGE op_id ///////////////////////
-    const [userEntry, setUserEntry] = useState<Thread>({ op_id: 1, title: '', content: '' });
+// routing into the different methods
+export default function HandleThread(method: { method: string }) {
+    const index = methods.indexOf(method.method);
+    if (index !== -1) {
+        return methodsFunctions[index]();
+    } else {
+        console.log("invalid method");
+        return NotFound({ errorStatus: 404 });
+    }
+}
+
+function HandleCreateThread() {
+    // op_id handled in controllers.tsx
+    const [userEntry, setUserEntry] = useState<Thread>({ title: '', content: '' });
     const [validEntry, setValidEntry] = useState<boolean>(true);
     const [errorMessage, setErrorMessage] = useState<string>('');
 
     let navigate = useNavigate();
 
-    async function HandlePostThread(event: FormEvent) {
-        event.preventDefault();
+    // useEffect(() => {
+    //     const token = localStorage.getItem("jwtToken");
 
+    //     if (token) {
+    //         try {
+    //             const decodedToken: any = jwtDecode(token);
+    //             const userId = decodedToken.UserID
+    //             console.log("userid: ", userId);
+    //             setUserEntry(prevState => ({ ...prevState, op_id: userId }));
+    //         } catch (error) {
+    //             console.error("Failed to decode JWT", error);
+    //             setErrorMessage("Invalid token or user not authenticated.");
+    //             setValidEntry(false);
+    //         }
+    //     } else {
+    //         setErrorMessage("User not authenticated. Please log in.");
+    //         setValidEntry(false);
+    //     }
+    // }, []);
+
+    async function HandleRequest(event: FormEvent) {
+        event.preventDefault();
         const { isValid, errorMessage } = ValidateThreadInput(userEntry);
+        console.log("valid: ", isValid);
         if (!isValid) {
             setErrorMessage(errorMessage);
             setValidEntry(isValid);
@@ -39,6 +72,7 @@ export function CreateThread() {
             window.location.reload();
         }
     }
+
     function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
         const { id, value } = event.target;
         setUserEntry(prevState => ({
@@ -52,7 +86,7 @@ export function CreateThread() {
             <title>Create Thread</title>
             <Box sx={PageBoxStyle}>
                 <Header></Header>
-                <form onSubmit={HandlePostThread}>
+                <form onSubmit={HandleRequest}>
                     <Box sx={{ marginTop: 1, marginBottom: 1, }}>
                         <TextField fullWidth label="Title" id="title" value={userEntry.title} onChange={handleInputChange} multiline />
                     </Box>
@@ -68,25 +102,30 @@ export function CreateThread() {
     );
 }
 
+function HandleUpdateThread() {
+    return (<></>);
+}
 
-const methods = ["create", "update", "delete"];
-const methodsFunctions = [CreateThread];
-
-export default function HandleThread(method: { method: string }) {
+function HandleDeleteThread() {
+    console.log("deleting...");
+    let postId = useParams().num;
+    console.log("postId: ", postId);
     let navigate = useNavigate();
-    const index = methods.indexOf(method.method);
-    if (index !== -1) {
-        return methodsFunctions[index]();
-    } else {
-        console.log("invalid method");
-        return NotFound({ errorStatus: 404 });
+    async function HandleRequest(postId: string) {
+        const response = await DeleteThread(postId);
+        if (response.success) {
+            console.log(`successfully deleted thread post id: ${postId}`);
+            navigate('/');
+            window.location.reload();
+        } else {
+            NotFound({ errorStatus: response.errorStatus as number });
+            window.location.reload();
+        }
     }
-}
 
-function UpdateThread() {
-    return;
-}
+    if (postId) {
+        HandleRequest(postId);
+    }
 
-function DeleteThread() {
-    return;
+    return (<></>);
 }
