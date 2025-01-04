d4aff1c08d4ad21efd8a5657114c386bdc26b60b
imrenzo <109291780+imrenzo@users.noreply.github.com>
2025-01-05 00:39:47 +0900

    added for delete thread operation

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -9,11 +9,14 @@ import (
 
 func SetUpRoutes(router chi.Router) {
 	router.Get("/", database.LoadPosts)
-	router.Get("/post_id/{num}", database.SinglePostAndComments)
+	router.Get("/post_id/{id}", database.SinglePostAndComments)
 	// router.Put("/post_id/{num}", database.SinglePostAndComments)
+	router.With(jwtHandler.TokenVerifyMiddleware).Post("/checkpostowner", database.CheckPostOwner)
+	
 	router.With(jwtHandler.TokenVerifyMiddleware).Post("/createThread", database.CreateThread)
+	router.With(jwtHandler.TokenVerifyMiddleware).Delete("/delete_thread/{id}", database.DeleteThread)
+	// user handling
 	router.Get("/authenticate", jwtHandler.DirectAuthenticate)
-	// router.Post("/createThread", database.CreateThread)
 	router.Post("/login", users.LogInUser)
 	router.Post("/signup", users.SignUpUser)
 }
