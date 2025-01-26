c29cb2651727c246a01edeef43882fe98a93421c
renzo <renzo.chua@yahoo.com.sg>
2025-01-26 21:10:57 +0800

    undo prev commit

--- server/main.go
+++ server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 
@@ -9,18 +8,8 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// render keeps having: No open ports detected, continuing to scan...
-// delaying go from running on load by very long, even though app already listening and serving a port below
-// so this ftn will tell render that app is working ok
-func healthCheck(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "OK")
-}
-
 func main() {
 	r := router.Setup()
-
-	http.HandleFunc("/health", healthCheck)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
